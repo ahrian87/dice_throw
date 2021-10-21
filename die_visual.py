36cfb5c6af6ba0f6e7b933d2b7a78cdc7ae3b4ea
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-21 22:13:23 +0200

    Added some results analysis - frequency of every roll result

--- die_visual.py
+++ die_visual.py
@@ -10,4 +10,10 @@ for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-print(results)
+# Results analysis
+frequencies = []
+for value in range(1, die.num_sides+1):
+    frequency = results.count(value)
+    frequencies.append(frequency)
+
+print(frequencies)
