5861cb31799f85e7c3f01860172d34ffb6a2334f
ahrian87 <maciej.zbydniewski@gmail.com>
2021-10-24 15:26:17 +0200

    Imported some classes and functions from plotly module, added code for visualizing the results

--- die_visual.py
+++ die_visual.py
@@ -1,19 +1,29 @@
 # sourcery skip: for-index-underscore
+from plotly.graph_objs import Bar, Layout
+from plotly import offline
+
 from die import Die
 
-# Creating a D6 die
+# Creating a D6 die.
 die = Die()
 
-# Making some rolls and adding the results to the list
+# Making some rolls and adding the results to the list.
 results = []
 for roll_num in range(1000):
     result = die.roll()
     results.append(result)
 
-# Results analysis
+# Results analysis.
 frequencies = []
 for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(frequencies)
+# Results visualization.
+x_values = list(range(1, die.num_sides+1))
+data = [Bar(x=x_values, y=frequencies)]
+
+x_axis_config = {"title": "Wynik"}
+y_axis_config = {"title": "Częstotliwość występowania wartości"}
+my_layout = Layout(title="Wynik rzucania pojedynczą kością D6 tysiąc razy", xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({"data": data, "layout": my_layout}, filename="d6.html")
